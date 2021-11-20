74052fc85e78cebe0e92f53d0f282cbe1267af92
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-11-20 22:24:35 +0700

    implement recursion

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,5 +1,7 @@
-//TODO 1 > PALINDROME CHECKER
-//A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+// TODO 1 > PALINDROME CHECKER
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+// my old solution (without recursion)
 function palindrome(str) {
   let regex = /[\W^_]/g;  //match all non-alphanumeric characters
   let newStr = str        //filter all non-alphanumeric characters and lowercase it
@@ -20,6 +22,20 @@ function palindrome(str) {
   return true;
 }
 
-console.log(palindrome("_eye"));  //eye, true
-console.log(palindrome("race car"));  //racecar, true
-console.log(palindrome("A man, a plan, a canal. Panama")); //amanaplanacanalpanama, true
+// my new solution (with recursion)
+function palindrome(str) {
+  if (str == '') {
+    return true;
+  }
+
+  const filteredStr = str
+    .replace(/[\W^_]/g, "")
+    .toLowerCase();
+
+  return filteredStr[0] == filteredStr.slice(-1) && palindrome(filteredStr.slice(1, -1));
+}
+
+console.log(palindrome("_eye"));                            //eye, true
+// console.log(palindrome("race car"));                        //racecar, true
+// console.log(palindrome("A man, a plan, a canal. Panama"));  //amanaplanacanalpanama, true
+// console.log(palindrome("hello"));                           //hello, false
